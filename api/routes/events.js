703ff7477518f4c459ba5d170c9d68923006f66f
Andrew Iguina <iguinaa@gmail.com>
2020-01-23 22:00:27 -0500

    Updated events POST to send a response.

--- api/routes/events.js
+++ api/routes/events.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 
 router.post('/events', function (req, res, next) {
     console.log('received an event')
+    res.status(202).json({
+        'message': 'event received'
+    })
 })
 
 module.exports = router
